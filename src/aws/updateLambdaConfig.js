6de49d9aec6be6c939f5c473f4068c6af661c973
jasonoverby <disguisekit@gmail.com>
2018-12-04 04:19:28 -0800

    allow option flags to be more intuitive

--- src/aws/updateLambdaConfig.js
+++ src/aws/updateLambdaConfig.js
@@ -3,6 +3,8 @@ const { promisify } = require('util');
 
 const { readConfig } = require('../util/fileUtils');
 const { bamWarn, bamError } = require('../util/logger');
+const bamSpinner = require('../util/spinner');
+const checkForOptionType = require('../util/checkForOptionType');
 const bamBam = require('../util/bamBam');
 const { doesRoleExist } = require('./doesResourceExist');
 const getLambda = require('./getLambda');
@@ -11,11 +13,22 @@ const apiVersion = 'latest';
 
 const dbRole = 'databaseBamRole'; // TODO -- refactor for testing
 
+const getRole = async (lambdaName) => {
+  try {
+    const data = await getLambda(lambdaName);
+    return data.Configuration.Role;
+  } catch (err) {
+    bamError(err);
+  }
+};
+
 module.exports = async function updateLambdaConfig(lambdaName, path, options) {
   const config = await readConfig(path);
   const { region, accountNumber, role } = config;
   const lambda = new AWS.Lambda({ apiVersion, region });
   const asyncLambdaUpdateFunctionConfiguration = promisify(lambda.updateFunctionConfiguration.bind(lambda));
+  const permitDb = checkForOptionType(options, 'permitDb');
+  const revokeDb = checkForOptionType(options, 'revokeDb');
 
   const databaseRoleArn = `arn:aws:iam::${accountNumber}:role/${dbRole}`;
   const defaultRoleArn = `arn:aws:iam::${accountNumber}:role/${role}`;
@@ -25,24 +38,17 @@ module.exports = async function updateLambdaConfig(lambdaName, path, options) {
     const userRole = options.role[0];
     const roleExists = await doesRoleExist(userRole);
     if (!roleExists) {
-      bamWarn(`Role ${userRole} does not exist`);
+      bamSpinner.stop();
+      bamWarn(`Role "${userRole}" does not exist`);
+      bamSpinner.start();
       return;
     }
     updatedRoleArn = `arn:aws:iam::${accountNumber}:role/${userRole}`;
-  } else if (options.revokeDb) {
+  } else if (revokeDb) {
     updatedRoleArn = defaultRoleArn;
-  } else if (options.permitDb) {
+  } else if (permitDb) {
     updatedRoleArn = databaseRoleArn;
   }
-
-  const getRole = async (lambdaName) => { 
-    try {
-      const data = await getLambda(lambdaName);
-      return data.Configuration.Role;
-    } catch (err) {
-      bamError(err);
-    }
-  };
 
   const currentRoleArn = await getRole(lambdaName);
   if (updatedRoleArn && currentRoleArn !== updatedRoleArn) {
@@ -50,6 +56,9 @@ module.exports = async function updateLambdaConfig(lambdaName, path, options) {
       FunctionName: lambdaName,
       Role: updatedRoleArn,
     };
-    await bamBam(asyncLambdaUpdateFunctionConfiguration, { params: [configParams], retryError: 'TooManyRequestsException'});
+
+    await bamBam(asyncLambdaUpdateFunctionConfiguration, {
+      params: [configParams], retryError: 'TooManyRequestsException',
+    });
   }
 };

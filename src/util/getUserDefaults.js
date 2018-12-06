910c68516642d780aceec832a88481b07fd8fc9a
jasonoverby <disguisekit@gmail.com>
2018-12-06 06:33:34 -0800

    remove region from getUserDefaults and isConfigured

--- src/util/getUserDefaults.js
+++ src/util/getUserDefaults.js
@@ -3,17 +3,10 @@ const getUserInput = require('./getUserInput');
 
 const { doesRoleExist } = require('../aws/doesResourceExist');
 
-const regions = ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2',
-  'ca-central-1', 'eu-central-1', 'eu-west-1', 'eu-west-2',
-  'eu-west-3', 'ap-northeast-1', 'ap-northeast-2', 'ap-northeast-3',
-  'ap-southeast-1', 'ap-southeast-2', 'ap-south-1', 'sa-east-1'];
-
 const badNum = 'Account number must have 12 numerical digits';
-const badRegion = `Region must be one of the following:\n${regions.join('\n')}`;
 const badRole = 'The role name you supplied is not registered to the account provided';
 
 const validNum = r => /^[0-9]{12}$/.test(r);
-const validRegion = r => regions.includes(r);
 
 module.exports = async function getUserDefaults(path) {
   const config = await readConfig(path);
@@ -28,13 +21,6 @@ module.exports = async function getUserDefaults(path) {
       defaultAnswer: process.env.AWS_ID,
     };
 
-    const getUserRegion = {
-      question: 'Please provide your default region: ',
-      validator: validRegion,
-      feedback: badRegion,
-      defaultAnswer: config.region,
-    };
-
     const getRole = {
       question: 'Please provide your default role (if you do not provide a role, one will be created for you): ',
       validator: validRole,
@@ -42,10 +28,10 @@ module.exports = async function getUserDefaults(path) {
       defaultAnswer: defaultRole,
     };
 
-    const configPrompts = [getAccountNumber, getUserRegion, getRole];
+    const configPrompts = [getAccountNumber, getRole];
 
     const userDefaults = await getUserInput(configPrompts); // undefined if user quits prompts
-    if (userDefaults) [config.accountNumber, config.region, config.role] = userDefaults;
+    if (userDefaults) [config.accountNumber, config.role] = userDefaults;
     return userDefaults;
   };
 
